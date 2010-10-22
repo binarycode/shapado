2347672458dd5b4e911a7d6ec8a52cdedc4972a2
Igor Sidorov <igor.cug@gmail.com>
2010-10-22 19:01:46 +0400

    fixed autologin
    This commit fixed following bug:
    If user logged in in external website, then visited Shapado and
    then logged in as another user, he will remain logged as first user
    in Shapado. Also when he logs out from external website, he remains
    logged in in Shapado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,30 +47,35 @@ class ApplicationController < ActionController::Base
 
   protected
   def auth_user
-    unless logged_in?
-      external_session_cookie = cookies[AppConfig.external_session]
-      return if external_session_cookie.blank?
+    logged_in?
+    # NOTE do not remove this line
+    # if it is removed, strange errors "undefined class/module User" appear
 
+    id = nil
+    external_session_cookie = cookies[AppConfig.external_session]
+    if external_session_cookie.present?
       external_session = Marshal.load(Base64.decode64(CGI.unescape(external_session_cookie.split("\n").join).split('--').first))
       id = external_session["warden.user.user.key"].try :[], 1
-      return if id.blank?
-
-      user = User.find_by_external_id id
-      if user.blank?
-        external_user = ExternalUser.find(id) rescue nil
-        return if external_user.blank?
-
-        user = User.create(
-         :login => external_user.login,
-         :email => external_user.email,
-         :identity_url => external_user.identity,
-         :role => "user",
-         :external_id => id
-        )
-      end
-
-      sign_in user if user.present?
     end
+
+    sign_out current_user if logged_in? && (id.blank? || current_user.external_id != id)
+    return if id.blank?
+
+    user = User.find_by_external_id id
+    if user.blank?
+      external_user = ExternalUser.find(id) rescue nil
+      return if external_user.blank?
+
+      user = User.create(
+       :login => external_user.login,
+       :email => external_user.email,
+       :identity_url => external_user.identity,
+       :role => "user",
+       :external_id => id
+      )
+    end
+
+    sign_in user if user.present?
   end
 
   def check_group_access
